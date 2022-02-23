edcdcc8af503cc153e4d1ae7dae2369dba9957ee
Rookie github <85553325+RookieLim@users.noreply.github.com>
2022-02-23 14:38:57 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,50 +1,74 @@
 import * as Location from 'expo-location';
-import React, { useEffect } from "react";
-import {View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Dimensions, StyleSheet, ScrollView, ActivityIndicator } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width:SCREEN_WIDTH } = Dimensions.get("window");
 
-console.log(SCREEN_WIDTH);
+const API_KEY="5045cbb6fdf605aa92353d8714870487";
+
+const icons = {
+  Clouds:"cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "Lightning",
+};
 
 export default function APP() {
-  const [location, setLocation] = useState();
+  const [city, setCity]= useState("Loading...");
+  const [days,setDays]= useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
-    const await Location.requestPermissionsAsync();
-  }; 
-  useEffect(() => {
-    ask();
-  }, []);
-  return (
+  const getWeather =async () =>{
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }   
+    const{coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false}
+  );
+  setCity(location[0].city); 
+  const response= await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`); 
+  const json = await response.json();
+  setDays(json.daily);  
+ };
+ useEffect(()=>{
+   getWeather();
+ },[]);
+ return (
   <View style={styles.container}>
      <View style={styles.city}>
-       <Text style={styles.cityName}>Seoul</Text>
+       <Text style={styles.cityName}>{city}</Text>
      </View>
      <ScrollView 
        pagingEnabled
        horizontal
        showsHorizontalScrollIndicator={false} 
-       ContentContainerStyle={styles.weather}>
-
-       <View style={styles.day}>
-         <Text style={styles.temp}>27</Text>
-         <Text style={styles.description}>Sunny</Text>
-       </View>
-       <View style={styles.day}>
-         <Text style={styles.temp}>27</Text>
-         <Text style={styles.description}>Sunny</Text>
-       </View>
-       <View style={styles.day}>
-         <Text style={styles.temp}>27</Text>
-         <Text style={styles.description}>Sunny</Text>
-       </View>
-       <View style={styles.day}>
-         <Text style={styles.temp}>27</Text>
-         <Text style={styles.description}>Sunny</Text>
-       </View>
+       ContentContainerStyle={styles.weather}
+     >
+      {days.length === 0 ? ( 
+      <View style={styles.day}>
+       <ActivityIndicator 
+       color="white" 
+       style={{marginTop:10}} 
+       size="large"
+       /> 
+      </View>
+      ) : (
+        days.map((day,index) => 
+        <View key={index} style={styles.day}>
+          <View>
+          <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+          <Fontisto name={icons[day.weather[0].main]} size={36} color="black" /></View>
+          <Text style={styles.description}>{day.weather[0].main}</Text>  
+          <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+        </View>
+        )
+      )}  
       </ScrollView>
      </View> 
-
    );
   }   
 
@@ -75,5 +99,8 @@ const styles =StyleSheet.create({
     marginTop: -30,
     fontSize:60, 
   },
+  tinyText: {
+    fontSize: 20,
+  }
 });
 
